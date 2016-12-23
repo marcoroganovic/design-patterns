f71632207147da5c83207a66b6263bc8e3bc81db
Marco Roganovic <marcoroganovic@gmail.com>
2016-12-23 22:29:37 +0100

    Implementation of Registry Pattern

---
+++ registry.js
@@ -0,0 +1,22 @@
+var Registry = Registry || (function() {
+  
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  function Registry(defaultVal) {
+    this._default = defaultVal;
+    this._values = Object.create(null);
+  }
+
+  Registry.prototype.register = function(name, val) {
+    this._values[name] = val;
+  }
+
+  Registry.prototype.get = function(name) {
+    return hasOwnProperty.call(this._values, name) ? this._values[name] : this._default;
+  }
+
+
+  return Registry;
+
+})();
+
