be33c6c37eacf9be8ac8f135932540ca80f72828
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-05 00:01:29 +0100

    observer implementation

---
+++ observer.js
@@ -0,0 +1,31 @@
+var Observer = Observer || (function() {
+  
+  function Observer() {
+    this.handlers = {};
+  }
+
+  Observer.prototype.subscribe = function(event, cb) {
+    this.handlers[event] = this.handlers[event] || [];
+    this.handlers[event].push(cb);
+  }
+
+  Observer.prototype.unsubscribe = function(event, cb) {
+    if(!this.handlers[event]) return;
+    this.handlers[event] = this.handlers[event].filter(fn => {
+      if(fn !== cb) return fn;
+    });
+  }
+
+  Observer.prototype.trigger = function(eventType, e) {
+    this.handlers[eventType].forEach(cb => cb(e));
+  }
+
+  Observer.prototype.flush = function(eventType) {
+    if(this.handlers[eventType]) {
+      this.handlers[eventType] = [];
+    }
+  }
+
+  return Observer;
+
+})();
